d66ff36d3c530ab230727e53e801e37b6aba9945
Paliao <andre_paliao@hotmail.com>
2018-02-04 23:45:14 -0200

    Adding mySQL connection

--- simple_concepts/app/routes/news.js
+++ simple_concepts/app/routes/news.js
@@ -1,5 +1,12 @@
+const databaseConnection = require('../../config/database')
+
 module.exports = function (app) {
+  const connection = databaseConnection()
+
   app.get('/news', function (req, res) {
-    res.render('news/news')
+    
+    connection.query('select * from news', function(error, result) {
+      res.render('news/news', { news: result })
+    })
   })
 }
